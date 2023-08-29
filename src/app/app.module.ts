46ab6cbb7387d243f358d9e3ef38e215ec046bed
Panatda Butwiangphan <butwiangphan38@gmail.com>
2023-08-29 14:21:51 +0700

    Revert "เพิ่ม routing"
    This reverts commit 5a3c5c8d454bbc0bf09e0c1ac7f6da4d5e9b4b99.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './user/user.component';
-import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent,
-    AdminComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
