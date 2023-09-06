13dda120a3fecd9799e9e84a63216323026e9444
SatananY <satananyindee@gmail.com>
2023-09-06 15:57:54 +0700

    update sign in

--- src/app/Service/User/user-sign-in.service.ts
+++ src/app/Service/User/user-sign-in.service.ts
@@ -1,21 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class UserRegisterService {
   constructor(private http: HttpClient) {}
 
-  // เพิ่มฟังก์ชัน userSignIn ในคลาส UserRegisterService
-  userSignIn(username: string, password: string) {
-    // ส่งคำขอ HTTP GET เพื่อตรวจสอบบัญชีผู้ใช้
+  userSignIn(email: string, password: string) {
+    const params = new HttpParams()
+      .set('email', email)
+      .set('password', password);
+
     return this.http.get<any>(
       'http://114.119.173.133:8080/insurtech/api/v1/user',
-      {
-        params: {
-          username: username,
-          password: password,
-        },
-      }
+      { params }
     );
   }
 }
--- src/app/home-page/sign-in/signin.component.ts
+++ src/app/home-page/sign-in/signin.component.ts
@@ -8,7 +8,7 @@ import { UserRegisterService } from 'src/app/Service/User/user-sign-in.service';
   styleUrls: ['./signin.component.css'],
 })
 export class SigninComponent {
-  username: string = '';
+  email: string = '';
   password: string = '';
 
   constructor(
@@ -17,33 +17,34 @@ export class SigninComponent {
   ) {}
 
   login() {
-    // ตรวจสอบว่าช่อง username และ password ไม่ว่าง
-    if (this.username.trim() !== '' && this.password.trim() !== '') {
+    // ตรวจสอบว่าช่อง email และ password ไม่ว่าง
+    if (this.email.trim() !== '' && this.password.trim() !== '') {
+      
       // เรียกใช้งาน userSignIn จาก UserRegisterService
-      this.userRegisterService
-        .userSignIn(this.username, this.password).subscribe(
-          (response) => {
-            // ตรวจสอบผลลัพธ์ที่ได้จากการเรียก userSignIn
-            if (response.username === this.username) {
-              // ถ้ามีบัญชีผู้ใช้ในระบบ ตรวจสอบว่าเป็นบัญชีผู้ดูแลระบบหรือไม่
-              if (this.username === 'ad2566' && this.password === 'ad6652') {
-                // ถ้าเป็นบัญชีผู้ดูแลระบบ นำผู้ใช้ไปยังหน้าหลักของ admin
-                this.router.navigate(['/menu']);
-              } else {
-                // ถ้าไม่ใช่บัญชีผู้ดูแลระบบ นำผู้ใช้ไปยังหน้าหลักของ user
-                this.router.navigate(['/user']);
-              }
-            } else {
-              // ถ้าไม่มีบัญชีผู้ใช้ในระบบ แจ้งเตือนผู้ใช้
-              alert('บัญชีผู้ใช้ไม่ถูกต้อง');
-            }
-          },
-          (error) => {
-            console.error('เกิดข้อผิดพลาดในการตรวจสอบบัญชีผู้ใช้', error);
-          }
-        );
+ this.userRegisterService.userSignIn(this.email, this.password).subscribe(
+   (response) => {
+     // ตรวจสอบผลลัพธ์ที่ได้จากการเรียก userSignIn
+     if ((response as any).userEmail === this.email) {
+       // ตรวจสอบบัญชีผู้ใช้
+       if (this.email === 'ad2566' && this.password === 'ad6652') {
+         // ถ้าเป็นบัญชีผู้ดูแลระบบ นำผู้ใช้ไปยังหน้าหลักของ admin
+         this.router.navigate(['/menu']);
+       } else {
+         // ถ้าไม่ใช่บัญชีผู้ดูแลระบบ นำผู้ใช้ไปยังหน้าหลักของ user
+         this.router.navigate(['/user']);
+       }
+     } else {
+       // ถ้าไม่มีบัญชีผู้ใช้ในระบบ แจ้งเตือนผู้ใช้
+       alert('บัญชีผู้ใช้ไม่ถูกต้อง');
+     }
+   },
+   (error) => {
+     console.error('เกิดข้อผิดพลาดในการตรวจสอบบัญชีผู้ใช้', error);
+   }
+ );
+
     } else {
-      // แจ้งเตือนผู้ใช้ว่าช่อง username หรือ password ว่าง
+      // แจ้งเตือนผู้ใช้ว่าช่อง email หรือ password ว่าง
       alert('กรุณากรอกชื่อผู้ใช้และรหัสผ่าน');
     }
   }
