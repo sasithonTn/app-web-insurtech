7b30a7be5886f4df4fde07f468647c9f700235c5
SatananY <satananyindee@gmail.com>
2023-09-07 18:02:56 +0700

    no message

--- src/app/home-page/reward/reward.component.ts
+++ src/app/home-page/reward/reward.component.ts
@@ -7,14 +7,16 @@ interface Country {
 @Component({
   selector: 'app-reward',
   templateUrl: './reward.component.html',
-  styleUrls: ['./reward.component.css']
+  styleUrls: ['./reward.component.css'],
 })
 export class RewardComponent {
   date: Date | undefined;
   items!: string[][];
-
+  
   ngOnInit() {
-      this.items = Array.from({ length: 1000 }).map((_, i) => Array.from({ length: 1000 }).map((_j, j) => `Item #${i}_${j}`));
+    this.items = Array.from({ length: 1000 }).map((_, i) =>
+      Array.from({ length: 1000 }).map((_j, j) => `Item #${i}_${j}`)
+    );
   }
   myArray = [
     { label: 'ผลรางวัล วันที่ 25/08/2566 เวลา 11:00 น.' },
@@ -22,8 +24,5 @@ export class RewardComponent {
     { label: 'ผลรางวัลวันที่ 3' },
     { label: 'ผลรางวัลวันที่ 3' },
     { label: 'ผลรางวัลวันที่ 3' },
-    
   ];
-  
-  
 }
